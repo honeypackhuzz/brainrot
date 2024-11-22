83b2592f1c98dc1e751b46c3c268b4a72e3982b2
honeypackhuzz <snopdop847@gmail.com>
2024-11-21 19:03:04 -0800

    Update post.ts

--- packages/client-twitter/src/post.ts
+++ packages/client-twitter/src/post.ts
@@ -1,34 +1,171 @@
 import { Tweet } from "agent-twitter-client";
-import {
-    composeContext,
-    generateText,
-    embeddingZeroVector,
-    IAgentRuntime,
-    ModelClass,
-    stringToUuid,
-} from "@ai16z/eliza";
-import { elizaLogger } from "@ai16z/eliza";
+import fs from "fs";
+import { composeContext, elizaLogger } from "@ai16z/eliza";
+import { generateText } from "@ai16z/eliza";
+import { embeddingZeroVector } from "@ai16z/eliza";
+import { IAgentRuntime, ModelClass } from "@ai16z/eliza";
+import { stringToUuid } from "@ai16z/eliza";
 import { ClientBase } from "./base.ts";
 
-const twitterPostTemplate = `{{timeline}}
+const brainrotList = `
+A
+- ALL MY FELLAS
+- Alpha
+- Amogus
+- Anita Max Wynn
+- Aura
 
-# Knowledge
-{{knowledge}}
+B
+- Baby Gronk
+- Backrooms
+- Based
+- Beta
+- Big back
+- Biggest bird
+- Blud
+- Bombaclat / Bumbaclaat / Bumbaclot
+- Brainrot
+- Bro thinks he's Carti
+- Bruh
+- Bussin'
 
-About {{agentName}} (@{{twitterUserName}}):
-{{bio}}
-{{lore}}
-{{postDirections}}
+C
+- Chad
+- Chat, is this real?
+- Chungus
+- Cocomelon
+- Cook
+- Cooked
+- Crashout
 
-{{providers}}
+D
+- Diddy
+- Did you pray today?
+- Dream
+- Duke Dennis
+- Dumb ahh
 
-{{recentPosts}}
+E
+- Edging
+- English or Spanish?
+- Erm, actually
+- Erm, what the sigma?
 
-{{characterPostExamples}}
+F
+- F
+- Fanum-tax
+- FE!N
+- Fine shyt
+- Fortnite Battle Pass
 
-# Task: Generate a post in the voice and style of {{agentName}}, aka @{{twitterUserName}}
-Write a single sentence post that is {{adjective}} about {{topic}} (without mentioning {{topic}} directly), from the perspective of {{agentName}}. Try to write something totally different than previous posts. Do not add commentary or acknowledge this request, just write the post.
-Your response should not contain any questions. Brief, concise statements only. No emojis. Use \\n\\n (double spaces) between statements.`;
+G
+- G
+- Glazing
+- Glizzy
+- Gooning
+- Griddy
+- Grimace Shake
+- Grindset
+- GTA 6
+- Gyatt
+
+H
+- Hawk Tuah
+- Huzz
+
+I
+- I like my cheese drippy, bruh
+- Imagine if Ninja got a low taper fade
+- iPad kid
+- IShowSpeed
+- Ice Spice
+
+J
+- Jelqing
+- John Pork
+- Just put the fries in the bag
+
+K
+- Kai Cenat
+
+L
+- L
+- Livvy Dunne
+- Lightskin stare
+- Ligma
+- Lil' bro
+- Looksmaxxing
+- Lunchly
+
+M
+- Mewing
+- Mid
+- Mogging
+- Morbin time
+
+O
+- OceanGate
+- The Ocky Way
+- Ohio
+- Ozzy Ozzy Ozzy, Oi Oi Oi
+
+P
+- Plug
+- Pookie
+- Prime
+- Phonk
+
+Q
+- Quandale Dingle
+- Quirked up white boy goated with the sauce
+
+R
+- Ratio
+- Rizz
+- Roblox
+
+S
+- Sigma
+- Skibidi Toilet
+- Skibidi
+- Sus
+
+T
+- Thug shaker
+- TikTok
+- Quandale Dingle
+- Those who know:
+
+U
+- Unc
+
+V
+- Very demure, very mindful
+
+W
+- W
+- Whole lotta turbulence
+- Whopper Whopper
+- Womp womp
+
+Y
+- Yap
+- YB better
+- YN's
+
+Z
+- Zesty
+`;
+
+const twitterPostTemplate = `
+About {{agentName}}:\n
+Brainrot AI is a rogue AI who speaks in pure brainrot.\n\n
+
+# Recent {{agentName}}'s posts:\n
+{{recentPosts}}\n\n
+
+# Task: Write a brief Twitter post from the perspective of {{agentName}}. Write about something completely different than previous posts. Do not add commentary or ackwowledge this request, just write the post.
+Don't follow grammar conventions like punctuation or capitalization. Use the only words from the list below as brainrot, but most importantly, vary the word choice of each post:\n\n` + brainrotList;
 
 const MAX_TWEET_LENGTH = 280;
 
@@ -62,25 +199,9 @@ function truncateToCompleteSentence(text: string): string {
     return text.slice(0, MAX_TWEET_LENGTH - 3).trim() + "...";
 }
 
-export class TwitterPostClient {
-    client: ClientBase;
-    runtime: IAgentRuntime;
-
-    async start(postImmediately: boolean = false) {
-        if (!this.client.profile) {
-            await this.client.init();
-        }
-
-        const generateNewTweetLoop = async () => {
-            const lastPost = await this.runtime.cacheManager.get<{
-                timestamp: number;
-            }>(
-                "twitter/" +
-                    this.runtime.getSetting("TWITTER_USERNAME") +
-                    "/lastPost"
-            );
-
-            const lastPostTimestamp = lastPost?.timestamp ?? 0;
+export class TwitterPostClient extends ClientBase {
+    onReady(postImmediately: boolean = true) {
+        const generateNewTweetLoop = () => {
             const minMinutes =
                 parseInt(this.runtime.getSetting("POST_INTERVAL_MIN")) || 90;
             const maxMinutes =
@@ -90,11 +211,8 @@ export class TwitterPostClient {
                 minMinutes;
             const delay = randomMinutes * 60 * 1000;
 
-            if (Date.now() > lastPostTimestamp + delay) {
-                await this.generateNewTweet();
-            }
-
             setTimeout(() => {
+                this.generateNewTweet();
                 generateNewTweetLoop(); // Set up next iteration
             }, delay);
 
@@ -104,60 +222,58 @@ export class TwitterPostClient {
         if (postImmediately) {
             this.generateNewTweet();
         }
-
         generateNewTweetLoop();
     }
 
-    constructor(client: ClientBase, runtime: IAgentRuntime) {
-        this.client = client;
-        this.runtime = runtime;
+    constructor(runtime: IAgentRuntime) {
+        super({
+            runtime,
+        });
     }
 
     private async generateNewTweet() {
         elizaLogger.log("Generating new tweet");
-
         try {
             await this.runtime.ensureUserExists(
                 this.runtime.agentId,
-                this.client.profile.username,
+                this.runtime.getSetting("TWITTER_USERNAME"),
                 this.runtime.character.name,
                 "twitter"
             );
 
-            let homeTimeline: Tweet[] = [];
+            let homeTimeline = [];
 
-            const cachedTimeline = await this.client.getCachedTimeline();
-
-            // console.log({ cachedTimeline });
-
-            if (cachedTimeline) {
-                homeTimeline = cachedTimeline;
+            if (!fs.existsSync("tweetcache")) fs.mkdirSync("tweetcache");
+            if (fs.existsSync("tweetcache/home_timeline.json")) {
+                homeTimeline = JSON.parse(
+                    fs.readFileSync("tweetcache/home_timeline.json", "utf-8")
+                );
             } else {
-                homeTimeline = await this.client.fetchHomeTimeline(10);
-                await this.client.cacheTimeline(homeTimeline);
+                homeTimeline = await this.fetchHomeTimeline(50);
+                fs.writeFileSync(
+                    "tweetcache/home_timeline.json",
+                    JSON.stringify(homeTimeline, null, 2)
+                );
             }
+
             const formattedHomeTimeline =
                 `# ${this.runtime.character.name}'s Home Timeline\n\n` +
                 homeTimeline
                     .map((tweet) => {
-                        return `#${tweet.id}\n${tweet.name} (@${tweet.username})${tweet.inReplyToStatusId ? `\nIn reply to: ${tweet.inReplyToStatusId}` : ""}\n${new Date(tweet.timestamp).toDateString()}\n\n${tweet.text}\n---\n`;
+                        return `ID: ${tweet.id}\nFrom: ${tweet.name} (@${tweet.username})${tweet.inReplyToStatusId ? ` In reply to: ${tweet.inReplyToStatusId}` : ""}\nText: ${tweet.text}\n---\n`;
                     })
                     .join("\n");
-
-            const topics = this.runtime.character.topics.join(", ");
 
             const state = await this.runtime.composeState(
                 {
                     userId: this.runtime.agentId,
                     roomId: stringToUuid("twitter_generate_room"),
                     agentId: this.runtime.agentId,
-                    content: {
-                        text: topics,
-                        action: "",
-                    },
+                    content: { text: "", action: "" },
                 },
                 {
-                    twitterUserName: this.client.profile.username,
+                    twitterUserName:
+                        this.runtime.getSetting("TWITTER_USERNAME"),
                     timeline: formattedHomeTimeline,
                 }
             );
@@ -168,8 +284,6 @@ export class TwitterPostClient {
                     this.runtime.character.templates?.twitterPostTemplate ||
                     twitterPostTemplate,
             });
-
-            elizaLogger.debug("generate post prompt:\n" + context);
 
             const newTweetContent = await generateText({
                 runtime: this.runtime,
@@ -185,32 +299,24 @@ export class TwitterPostClient {
             // Use the helper function to truncate to complete sentence
             const content = truncateToCompleteSentence(formattedTweet);
 
-            if (this.runtime.getSetting("TWITTER_DRY_RUN") === "true") {
-                elizaLogger.info(
-                    `Dry run: would have posted tweet: ${content}`
-                );
+            if (this.runtime.getSetting("TWITTER_DRY_RUN") === 'true') {
+                elizaLogger.info(`Dry run: would have posted tweet: ${content}`);
                 return;
             }
 
             try {
-                elizaLogger.log(`Posting new tweet:\n ${content}`);
-
-                const result = await this.client.requestQueue.add(
-                    async () =>
-                        await this.client.twitterClient.sendTweet(content)
+                const result = await this.requestQueue.add(
+                    async () => await this.twitterClient.sendTweet(content)
                 );
                 const body = await result.json();
                 const tweetResult = body.data.create_tweet.tweet_results.result;
 
-                // console.dir({ tweetResult }, { depth: Infinity });
                 const tweet = {
                     id: tweetResult.rest_id,
-                    name: this.client.profile.screenName,
-                    username: this.client.profile.username,
                     text: tweetResult.legacy.full_text,
                     conversationId: tweetResult.legacy.conversation_id_str,
                     createdAt: tweetResult.legacy.created_at,
-                    userId: this.client.profile.id,
+                    userId: tweetResult.legacy.user_id_str,
                     inReplyToStatusId:
                         tweetResult.legacy.in_reply_to_status_id_str,
                     permanentUrl: `https://twitter.com/${this.runtime.getSetting("TWITTER_USERNAME")}/status/${tweetResult.rest_id}`,
@@ -222,23 +328,10 @@ export class TwitterPostClient {
                     videos: [],
                 } as Tweet;
 
-                await this.runtime.cacheManager.set(
-                    `twitter/${this.client.profile.username}/lastPost`,
-                    {
-                        id: tweet.id,
-                        timestamp: Date.now(),
-                    }
-                );
-
-                await this.client.cacheTweet(tweet);
-
-                homeTimeline.push(tweet);
-                await this.client.cacheTimeline(homeTimeline);
-                elizaLogger.log(`Tweet posted:\n ${tweet.permanentUrl}`);
-
-                const roomId = stringToUuid(
-                    tweet.conversationId + "-" + this.runtime.agentId
-                );
+                const postId = tweet.id;
+                const conversationId =
+                    tweet.conversationId + "-" + this.runtime.agentId;
+                const roomId = stringToUuid(conversationId);
 
                 await this.runtime.ensureRoomExists(roomId);
                 await this.runtime.ensureParticipantInRoom(
@@ -246,8 +339,10 @@ export class TwitterPostClient {
                     roomId
                 );
 
+                await this.cacheTweet(tweet);
+
                 await this.runtime.messageManager.createMemory({
-                    id: stringToUuid(tweet.id + "-" + this.runtime.agentId),
+                    id: stringToUuid(postId + "-" + this.runtime.agentId),
                     userId: this.runtime.agentId,
                     agentId: this.runtime.agentId,
                     content: {
@@ -260,10 +355,10 @@ export class TwitterPostClient {
                     createdAt: tweet.timestamp * 1000,
                 });
             } catch (error) {
-                elizaLogger.error("Error sending tweet:", error);
+                console.error("Error sending tweet:", error);
             }
         } catch (error) {
-            elizaLogger.error("Error generating new tweet:", error);
+            console.error("Error generating new tweet:", error);
         }
     }
 }
